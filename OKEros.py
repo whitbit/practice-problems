8e5f81151569868365cb699dc1853f3a62ea5c06
whitbit <whitbyte@gmail.com>
2018-01-03 17:11:09 -0800

    completed overlapping rects

--- OKEros.py
+++ OKEros.py
@@ -18,32 +18,50 @@ my_rectangle2 = {
     'height': 3,
 }
 
-
-def get_rectangular_intersection(rect1, rect2):
+def find_range_overlap(point1, length1, point2, length2):
     """
-
-    Find rectangular intersection of two given love rectangles.
-    Rectangles are defined as dictionaries.
+    Find overlap of x and y on a 1-D line.
 
     """
+    highest_start_point = max(point1, point2)
+    lowest_end_point = min(point1 + length1, point2 + length2)
 
-def find_x_overlap(rect1, rect2):
+    if highest_start_point >= lowest_end_point:
+        return (None, None)
 
-    first_x_range = (rect1['left_x'], rect1['left_x'] + rect1['width'])
-    print first_x_range
+    overlap_length = lowest_end_point - highest_start_point
 
-    second_x_range = (rect2['left_x'], rect2['left_x'] + rect2['width'])
-    print second_x_range
-
-    if first_x_range[1] <= second_x_range[0]:
-        return 'No overlap'
-
-    return (max(first_x_range[0], second_x_range[0]),
-            min(first_x_range[1], second_x_range[1]))
-
-print find_x_overlap(my_rectangle, my_rectangle2)
-
-def find_x_overlap(x1, width1, x2, width2):
-    highest_start_point = max(x1, x2)
-    lowest_end_point
+    return (highest_start_point, overlap_length)
     
+
+def find_rectangular_overlap(rect1, rect2):
+    """
+    >>> find_rectangular_overlap(my_rectangle, my_rectangle2) == { 'left_x': 2, 'bottom_y': 1, 'width': 4, 'height': 3}
+    True
+
+    """
+
+    x_overlap_point, x_overlap_width = find_range_overlap(
+        rect1['left_x'], rect1['width'], rect2['left_x'], rect2['width'])
+    y_overlap_point, y_overlap_height = find_range_overlap(
+        rect1['bottom_y'], rect1['height'], rect2['bottom_y'], rect2['height'])
+
+    if not x_overlap_width or not y_overlap_height: 
+        return {
+            'left_x': None,
+            'bottom_y': None,
+            'width': None,
+            'height': None
+        }
+
+    return {
+        'left_x': x_overlap_point,
+        'bottom_y': y_overlap_point,
+        'width': x_overlap_width,
+        'height': y_overlap_height
+    }
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
